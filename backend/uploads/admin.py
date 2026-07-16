6faa363988bf1ec959d3678bf8c472dde2962ed6
omen-katy404 <pong34811@gmail.com>
2026-07-16 16:41:40 +0700

    feat: register YouTubeAppConfig in Django Admin

--- backend/uploads/admin.py
+++ backend/uploads/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Destination, UploadJob
+from .models import Destination, UploadJob, YouTubeAppConfig
 
-admin.site.register(Destination)
-admin.site.register(UploadJob)
+
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = ("platform", "name", "is_active", "created_at")
+
+
+@admin.register(UploadJob)
+class UploadJobAdmin(admin.ModelAdmin):
+    list_display = ("title", "destination", "status", "created_at")
+
+
+@admin.register(YouTubeAppConfig)
+class YouTubeAppConfigAdmin(admin.ModelAdmin):
+    list_display = ("client_id", "redirect_uri")
